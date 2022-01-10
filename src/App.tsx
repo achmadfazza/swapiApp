76d203c07501591eff5bb3a451fb98d9630d4406
faza <arkadyfaza@gmail.com>
2022-01-11 04:00:08 +0700

    add all aps

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+  const [planets, setPlanets] = useState<any[]>([])
+  const [pageNumber, setPageNumber] = useState(1)
+
+  const fetchApi = async (pageNumber: any) => {
+    const res = await fetch(`https://swapi.dev/api/planets?page=${pageNumber}`)
+    const data = await res.json()
+    setPlanets(data.results)
+    console.log(data.results);
+  }
+  useEffect(() => {
+    fetchApi(pageNumber)
+  }, [pageNumber])
+
+  const loadMore = () => {
+    setPageNumber(prevPageNumber => prevPageNumber + 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Infinete Scroll</h1>
+      {
+        planets.map((planet, index) => (
+          <div className="planet" key={index}>
+            <h2>planet : {planet.name}</h2>
+            <h2>climate : {planet.climate}</h2>
+            <h2>created : {planet.created}</h2>
+          </div>
+        ))
+      }
+      <div className="loading">
+        <h3>{planets.length}</h3>
+        <button
+          onClick={loadMore} >
+          Load More
+        </button>
+      </div>
     </div>
   );
 }
